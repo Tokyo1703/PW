fc1aa4e1dea3cb2d6b86aed7d36d368128db6a29
Manuel <manuelantomuri@gmail.com>
2023-10-02 18:15:38 +0200

    Implementada clase Monitor

--- Campamento/src/App.java
+++ Campamento/src/App.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 public class App {
     public static void main(String[] args){
         Asistente Andres = new Asistente(21,"Andres Sanchez",Date.valueOf("2023-10-01"),true);
-        System.out.println(Andres.toString());
+        Monitor Carlos = new Monitor(32, "Carlos Ruiz Martinez", true);
+        System.out.println(Andres.toString() + "\n" + Carlos.toString());
     }
 }
